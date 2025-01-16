ee216c87cb5a84cf67928cc31d2db82dee65be21
Satish <mahatosatish463@gmail.com>
2025-01-16 20:58:24 +0545

    work on our story

--- frontend/src/components/WhoWeAreComponents/HowItStarted/HowItStarted.jsx
+++ frontend/src/components/WhoWeAreComponents/HowItStarted/HowItStarted.jsx
@@ -24,7 +24,7 @@ const HowItStarted = () => {
                 <div className="how_it_start_title">
                     <h1>Our Story</h1>
                 </div>
-                <div className="how_it_start_grid">
+                <div className="how_it_start_desktop how_it_start_grid">
                     <div className="how_it_start_box">
                         <div className="how_it_start_image">
                             <img src={data.first_image} alt="Story" />
@@ -41,6 +41,33 @@ const HowItStarted = () => {
                             <img src={data.second_image} alt="Story" />
                         </div>
                     </div>
+                    <div className="how_it_start_box">
+                        <div className="how_it_start_image">
+                            <img src={data.third_image} alt="Story" />
+                        </div>
+                    </div>
+                    <div className="how_it_start_content">
+                        <p>{parse(data.third_description)}</p>
+                    </div>
+                </div>
+                <div className="how_it_start_mobile how_it_start_grid">
+                    <div className="how_it_start_box">
+                        <div className="how_it_start_image">
+                            <img src={data.first_image} alt="Story" />
+                        </div>
+                    </div>
+                    <div className="how_it_start_content">
+                        <p>{parse(data.first_description)}</p>
+                    </div>
+                    <div className="how_it_start_box">
+                        <div className="how_it_start_image">
+                            <img src={data.second_image} alt="Story" />
+                        </div>
+                    </div>
+                    <div className="how_it_start_content">
+                        <p>{parse(data.second_description)}</p>
+                    </div>
+                    
                     <div className="how_it_start_box">
                         <div className="how_it_start_image">
                             <img src={data.third_image} alt="Story" />
